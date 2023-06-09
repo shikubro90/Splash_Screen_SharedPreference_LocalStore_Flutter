535155442aec8e14ad2deec0c573ded7a0cf96f9
Administrator <shikubro90@gmail.com>
2023-06-09 21:47:51 +0600

    Update

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,7 +11,16 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Hello"),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Center(child: Text("Welcome To Home", style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),)),
+            )
+          ],
+        ),
+      )
     );
   }
 }
--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:splashscreen/home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -9,65 +10,87 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final userEmailController = TextEditingController();
+  final userPasswordController = TextEditingController();
+  final userAge = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Center(
-          child: Text("Login"),
-        ),
-        automaticallyImplyLeading: false,
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Email"
-                )
-              ),
-
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Password"
-                ),
-              )
-
-              ,InkWell(
-                onTap: (){
-
-                },
-                child: Container(
-                  height: 50,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(5)
-
-                  ),
-                  child: const Center(child: Text("Login", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),)),
-                ),
-              )
-            ],
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Center(
+            child: Text(
+              "Login",
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
           ),
+          automaticallyImplyLeading: false,
         ),
-      )
-    );
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextFormField(decoration: InputDecoration(hintText: "Email"),keyboardType: TextInputType.text,),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(hintText: "Password"),
+                  keyboardType: TextInputType.text,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  controller: userAge,
+                  decoration: InputDecoration(hintText: "Age"),
+                  keyboardType: TextInputType.number,
+                ),
+                InkWell(
+                  onTap: () async {
+                    SharedPreferences pref =
+                        await SharedPreferences.getInstance();
+
+                    pref.setString("email", userEmailController.text.toString());
+                    pref.setString("password", userPasswordController.text.toString());
+                    pref.setString("age", userAge.text.toString());
+                    pref.setBool('status', true);
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeScreen()));
+
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(top: 20),
+                    height: 50,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(5)),
+                    child: const Center(
+                        child: Text(
+                      "Login",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    )),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 
-
-  Future<void> shiku() async{
+  Future<void> shiku() async {
     Future.delayed(Duration(seconds: 3));
     print("SHiku print");
   }
-
-
 }
